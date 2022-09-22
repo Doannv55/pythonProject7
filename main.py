802fb56b066318ef8651d16735fff92be51864ce
doann <doann@DESKTOP-QHT3D5L>
2022-09-22 23:25:22 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('123.jpg',0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
